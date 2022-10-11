43d301d7d8ccdabbfda9055cf2d1cc581c82d443
David Vorona <davidavorona@gmail.com>
2022-10-10 20:29:06 -0700

    Fix cron time for punishment reminder

--- src/constants.ts
+++ src/constants.ts
@@ -29,5 +29,5 @@ export const CRON_TIME = {
     ONBOARDING: "0 10,22 * * *",                // Every day at 10am and 10pm
     NEW_WEEK: "0 9 * * 2",                      // Every Tuesday at 9am
     PUNISHMENT_PICKER: "0 9 * * 3",             // Every Wednesday at 9am
-    PUNISHMENT_REMIND: "0 9 * * 1,3,4,5,6,7"    // Every day except Tuesday at 9am
+    PUNISHMENT_REMIND: "0 9 * * 0,1,3,4,5,6"    // Every day except Tuesday at 9am
 } as const;
